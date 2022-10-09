5f52c8e34371b5581cb8369a2d65f2d535c04cba
adamantdude <adam.plee23@gmail.com>
2022-10-09 17:26:56 -0500

    project framework

--- server/routes/list.router.js
+++ server/routes/list.router.js
@@ -2,12 +2,4 @@ const express = require('express');
 const router = express.Router();
 const pool = require('../modules/pool');
 
-router.get();
-
-router.post();
-
-router.put();
-
-router.delete();
-
 module.exports = router;
