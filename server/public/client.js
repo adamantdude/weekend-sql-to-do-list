1318c218f70151c90920c79b2a760fa39d285a38
adamantdude <adam.plee23@gmail.com>
2022-10-09 19:26:48 -0500

    implement listeners, handlers, and ajax promises in client.js ; correct index.html and add styling in the stylesheet ; code endpoints in list.router and test all for correct interactions with database

--- server/public/client.js
+++ server/public/client.js
@@ -6,7 +6,44 @@ function main() {
 }
 
 function render() {
-
+    $('#listBody').empty();
+    $.ajax({
+        url: '/list',
+        method: 'GET'
+    })
+    .then((res) => {
+        for(const task of res) {
+            if(task.isComplete) {
+                $('#listBody').append(`
+                    <tr class="complete">
+                        <td>${task.task}</td>
+                        <td>${task.isComplete}</td>
+                        <td></td>
+                        <td>
+                            <button class="deleteBtn" data-id="${task.id}">Delete</button>
+                        </td>
+                    </tr>
+                `)
+            }
+            else {
+                $('#listBody').append(`
+                    <tr>
+                        <td>${task.task}</td>
+                        <td>${task.isComplete}</td>
+                        <td>
+                            <button class="completeBtn" data-id="${task.id}">Complete</button>
+                        </td>
+                        <td>
+                            <button class="deleteBtn" data-id="${task.id}">Delete</button>
+                        </td>
+                    </tr>
+                `)
+            }
+        }
+    })
+    .catch(err => {
+        console.log('unable to get database info', err);
+    })
 }
 
 function handleListeners() {
@@ -16,13 +53,55 @@ function handleListeners() {
 }
 
 function onAdd() {
-    console.log('in Add');
+
+    let textObj = {
+        text: $('#taskText').val()
+    }
+
+    $('#taskText').val('');
+
+    $.ajax({
+        url: '/list',
+        method: 'POST',
+        data: textObj
+    })
+    .then(res => {
+        render();
+    })
+    .catch(err => {
+        console.log('unable to POST to database', err);
+    })
 }
 
 function onComplete() {
+    let listId = $(this).data('id');
+
+    $.ajax({
+        method: 'PUT',
+        url: `/list/${listId}`
+    })
+    .then(res => {
+        console.log('in onComplete');
+        render();
+    })
+    .catch(err => {
+        console.log('in onComplete error', err);
+    })
 
 }
 
 function onDelete() {
+    let listId = $(this).data('id');
 
+    $.ajax({
+        url: `/list/${listId}`,
+        method: 'DELETE'
+    })
+    .then(res => {
+        console.log('in onDelete');
+        render();
+    })
+    .catch(err => {
+        console.log('in onDelete error', err);
+    })
 }
